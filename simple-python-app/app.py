5b9649ba8137a61f0337c1aa653612caa55db435
Benny <132187364+Benny-17@users.noreply.github.com>
2025-03-07 02:09:07 +0530

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Benny!?!?!!'
+    return 'Hello, Benny pius!?!?!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # ✅ Make sure Flask runs on all interfaces
